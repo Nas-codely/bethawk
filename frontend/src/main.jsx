d493a14e603c0b2ecf342052608b149c5575197c
Nas-codely <890897eekh@01>
2025-04-14 05:43:49 +0100

    Fully added working frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BetHawkDashboard from './pages/BetHawkDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BetHawkDashboard />
+  </React.StrictMode>
+)
